8d7c16c4eba0b19d2fd53c3306a2c8259a7d401c
igarasy <lucas.igarashi@caju.com.br>
2024-03-15 13:00:18 -0300

    Initial Commit

---
+++ friend_diet Watch App/friend_dietApp.swift
@@ -0,0 +1,17 @@
+//
+//  friend_dietApp.swift
+//  friend_diet Watch App
+//
+//  Created by Lucas on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct friend_diet_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ friend_diet/friend_dietApp.swift
@@ -0,0 +1,17 @@
+//
+//  friend_dietApp.swift
+//  friend_diet
+//
+//  Created by Lucas on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct friend_dietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
